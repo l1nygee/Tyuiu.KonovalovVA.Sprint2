85d7f386de3304e551283f99f7de599bdbacf657
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-10-30 01:01:55 +0500

    Add project files.

---
+++ Tyuiu.KonovalovVA.Sprint2.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KonovalovVA.Sprint2.Task0.V22.Lib
+{
+    public class DataService : ISprint2Task0V22
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x == y;
+            res[1] = x != y;
+            res[2] = x < y+4000;
+            res[3] = x > y; 
+            res[4] = x <= y+4000;
+            res[5] = x-3050 >= y ;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KonovalovVA.Sprint2.Task0.V22/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KonovalovVA.Sprint2.Task0.V22.Lib;
+namespace Tyuiu.KonovalovVA.Sprint2.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 3105;
+            int y = 77;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.Title = "Спринт #2 | Выполнил: Коновалов В.А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Коновалов Владимир Артёмович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
