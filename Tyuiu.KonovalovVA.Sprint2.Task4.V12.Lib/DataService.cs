f9bd7e8d61b5897ded02e4fa2cdfa472a9cb0683
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-10-31 01:50:23 +0500

    Task4

--- Tyuiu.KonovalovVA.Sprint2.Task4.V12.Lib/DataService.cs
+++ Tyuiu.KonovalovVA.Sprint2.Task4.V12.Lib/DataService.cs
@@ -5,7 +5,9 @@ namespace Tyuiu.KonovalovVA.Sprint2.Task4.V12.Lib
     {
         public double Calculate(double x, double y)
         {
-            return Math.Round((Math.Sqrt(x) > y * 2) ? Math.Pow((7 + 2 / (y * y)), x) : (3*x*x- Math.Cos(y)* Math.Cos(y) + 10) / (y*y - Math.Sin(x) * Math.Sin(x) + 12),3);
+            //return Math.Round((Math.Sqrt(x) > y * 2) ? Math.Pow((7 + 2 / (y * y)), x) : (3*x*x-Math.Pow(Math.Cos(y),2)+10) / (y*y - Math.Pow(Math.Sin(x),2)+12),3);
+            //return Math.Round((Math.Sqrt(x) > y * 2) ? Math.Pow((7 + 2 / (y * y)), x) : (3*x*x- Math.Cos(y)* Math.Cos(y) + 10) / (y*y - Math.Sin(x) * Math.Sin(x) + 12),3);
+            return Math.Round((Math.Sqrt(x) > y * 2) ? Math.Pow((7 + 2 / (y * y)), x) : (3*x*x-Math.Pow(Math.Cos(y),2)+10) / (y*y - Math.Sin(x*x)+12),3);
         }
     }
 }
