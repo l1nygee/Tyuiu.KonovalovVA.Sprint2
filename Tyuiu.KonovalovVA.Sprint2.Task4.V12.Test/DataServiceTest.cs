e4bb8b45099809b90c5c32276b4a81d46544a420
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-10-31 01:49:24 +0500

    Task4

--- Tyuiu.KonovalovVA.Sprint2.Task4.V12.Test/DataServiceTest.cs
+++ Tyuiu.KonovalovVA.Sprint2.Task4.V12.Test/DataServiceTest.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.KonovalovVA.Sprint2.Task4.V12.Test
         [TestMethod]
         public void CalculateValid()
         {
-            double x = 1;
-            double y = 1;
+            double x = 9;
+            double y = 4;
             DataService ds = new DataService();
             double wait  = ds.Calculate(x, y);
             Assert.AreEqual(1.034,wait);
